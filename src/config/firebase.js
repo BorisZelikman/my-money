73e7ab53dd8e4621533ad2990a7444e95c8c92dd
BorisZelikman <oborzelik@gmail.com>
2023-09-02 09:48:43 +0300

    clean project (react + firebase)

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import apiKey from "./firebase-config.json";
+import authDomain from "./firebase-config.json";
+import projectId from "./firebase-config.json";
+import storageBucket from "./firebase-config.json";
+import messagingSenderId from "./firebase-config.json";
+import appId from "./firebase-config.json";
+import measurementId from "./firebase-config.json";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAER4YlW1J9DiTXEATr9aTnkQdK4TGFf68",
-  authDomain: "my-money-1d617.firebaseapp.com",
-  projectId: "my-money-1d617",
-  storageBucket: "my-money-1d617.appspot.com",
-  messagingSenderId: "989105357376",
-  appId: "1:989105357376:web:01df465df3875f857d55a5",
-  measurementId: "G-2PX2F7JL3T",
+  apiKey: `${apiKey.apiKey}`,
+  authDomain: `${authDomain.authDomain}`,
+  projectId: `${projectId.projectId}`,
+  storageBucket: `${storageBucket.storageBucket}`,
+  messagingSenderId: `${messagingSenderId.messagingSenderId}`,
+  appId: `${appId.appId}`,
+  measurementId: `${measurementId.measurementId}`,
 };
 
 const app = initializeApp(firebaseConfig);
