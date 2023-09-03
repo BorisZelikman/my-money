f105f9ae14b1414036516f21d74609b6afcd6c79
BorisZelikman <oborzelik@gmail.com>
2023-09-03 12:35:10 +0300

    added framer-motion, animated Logo created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import { Auth } from "./components/Auth";
+import { Logo } from "./components/Logo";
 
 function App() {
   return (
     <div className="App">
+      <Logo />
       <Auth />
     </div>
   );
