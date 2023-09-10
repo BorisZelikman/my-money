6885ae6d3857755b1ade5e28a1a29ca03effecff
BorisZelikman <oborzelik@gmail.com>
2023-09-10 16:54:36 +0300

    refactoring app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import { Auth } from "./routes/Auth";
+import {Auth} from "./routes/Auth";
 import Box from "@mui/material/Box";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Registration } from "./routes/Registration";
-import { UserProfile } from "./routes/UserProfile";
-import BalanceComponent from "./components/Items/BalanceComponent";
-import AddComponent from "./components/Items/AddComponent";
-import { Operations } from "./routes/Operations";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {Registration} from "./routes/Registration";
+import {UserProfile} from "./routes/UserProfile";
+import {Operations} from "./routes/Operations";
+import Balance from "./components/Items/Balance";
+import {AddAsset} from "./components/Items/AddAsset";
 
 export const App = () => {
   return (
@@ -26,8 +26,10 @@ export const App = () => {
           <Route path="/user-profile/:userId" element={<UserProfile />} />
           <Route path="/operations/:userId" element={<Operations />} />
 
-          <Route path="/balance" element={<BalanceComponent />} />
-          <Route path="/add" element={<AddComponent />} />
+
+
+          <Route path = "/user-profile/:userId/balance" element = {<Balance/>}/>
+          <Route path = "/user-profile/:userId/balance/add" element = {<AddAsset/>}/>
         </Routes>
       </Box>
     </Router>
