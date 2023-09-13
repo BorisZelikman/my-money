aee1c6634679692c9b8ae655e3d55c54129b2d90
BorisZelikman <oborzelik@gmail.com>
2023-09-13 14:37:07 +0300

    adding new user to users collection on registration, name field in Registration page

--- src/routes/UserProfile.js
+++ src/routes/UserProfile.js
@@ -1,53 +1,21 @@
-import React, {useEffect, useState} from "react";
-import {signOut} from "firebase/auth";
-import {auth} from "../config/firebase";
-import Box from "@mui/material/Box";
+import React, {useEffect} from "react";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import {Link, useNavigate} from "react-router-dom";
 import Stack from "@mui/material/Stack";
+import { useParams } from "react-router-dom";
+import { useUserPreference} from "../hooks/useUserPreference";
+import {Balance} from "../components/Items/Balance";
 
 export const UserProfile = () => {
-    const [user, setUser] = useState(null);
-
+    const { userId } = useParams()
+    const {userPreference, getUserPreference} = useUserPreference()
     useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((currentUser) => {
-            if (currentUser) {
-                setUser(currentUser);
-            } else {
-                setUser(null);
-            }
-        });
-        return () => {
-            unsubscribe();
-        };
+        getUserPreference(userId)
     }, []);
 
-    const logOut = async () => {
-        try {
-            await signOut(auth);
-        }
-        catch (err) {
-            console.error(err);
-        }
-    };
-
     return (
-        <Box sx = {{display: "flex", flexDirection: "column", alignItems: "center"}}>
-            {user ? (
-                <Stack spacing = {3}>
-                    <Typography variant = "h4">Welcome, {user.email}</Typography>
-                    <Typography variant = "h6">User ID: {user.uid}</Typography>
-
-                </Stack>
-            ) : (
-                <Stack spacing = {3}>
-                    <Typography variant = "h4">Please sign in to view your profile</Typography>
-                    <Button>
-                        <Link style = {{textDecoration: "none"}} to = "/">Back to sign in page</Link>
-                    </Button>
-                </Stack>
-            )}
-        </Box>
+        <Stack spacing = {4}>
+            <Typography variant = "h4">Welcome, {userPreference?.name}</Typography>
+            <Balance/>
+        </Stack>
     );
 };
