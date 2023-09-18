128d48246ab6e03c73580a5856a58a81520c670d
LDubersht <ldubersht@gmail.com>
2023-09-18 22:22:57 +0300

    Correct navBar

--- src/components/Items/NavigationBar.js
+++ src/components/Items/NavigationBar.js
@@ -5,32 +5,37 @@ import {auth} from "../../config/firebase";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import ButtonGroup from "@mui/material/ButtonGroup";
+import Typography from "@mui/material/Typography";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
-import BalanceIcon from "@mui/icons-material/Balance";
 import PriceChangeIcon from "@mui/icons-material/PriceChange";
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import StackedBarChartIcon from "@mui/icons-material/StackedBarChart";
+import EuroSymbolIcon from "@mui/icons-material/EuroSymbol";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import {observer} from "mobx-react";
 import AuthStore from "../../Stores/AuthStore";
 
 
 export const NavigationBar = observer(() => {
-  const [showNavBar, setShowNavBar] = useState(true);
-  const location = useLocation();
-  const [userID, setUserID] = useState(null);
+    const [showNavBar, setShowNavBar] = useState(true);
+    const location = useLocation();
+    const [userID, setUserID] = useState(null);
+    const isSmallHeightScreen = useMediaQuery("(max-height: 400px)");
+    const isMediumWidthScreen = useMediaQuery("(min-width: 701px)");
 
     useEffect(() => {
-        setShowNavBar(location.pathname !== "/registration" && location.pathname !== "/");
+        setShowNavBar(
+            location.pathname !== "/registration" && location.pathname !== "/"
+        );
         setUserID(AuthStore.currentUserID);
     }, [location.pathname]);
 
     const logOut = async () => {
         try {
-            
             await signOut(auth);
             AuthStore.clearStorage();
- 
+
 
         }
         catch (err) {
