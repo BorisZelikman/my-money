21b65763679e57879ef0a4fbfb48b5e81462e8ad
BorisZelikman <oborzelik@gmail.com>
2023-10-16 00:49:18 +0300

    dnd

--- src/components/Items/Balance.js
+++ src/components/Items/Balance.js
@@ -14,7 +14,43 @@ import {Grid} from "@mui/material";
 import {getExchangeRates} from "../../data/exchangeMethods";
 import { DragDropContext, Droppable, Draggable  } from 'react-beautiful-dnd';
 
+const DATA = [
+    {
+        id: "0e2f0db1-5457-46b0-949e-8032d2f9997a",
+        name: "Walmart",
+        items: [
+            { id: "26fd50b3-3841-496e-8b32-73636f6f4197", name: "3% Milk" },
+            { id: "b0ee9d50-d0a6-46f8-96e3-7f3f0f9a2525", name: "Butter" },
+        ],
+        tint: 1,
+    },
+    {
+        id: "487f68b4-1746-438c-920e-d67b7df46247",
+        name: "Indigo",
+        items: [
+            {
+                id: "95ee6a5d-f927-4579-8c15-2b4eb86210ae",
+                name: "Designing Data Intensive Applications",
+            },
+            { id: "5bee94eb-6bde-4411-b438-1c37fa6af364", name: "Atomic Habits" },
+        ],
+        tint: 2,
+    },
+    {
+        id: "25daffdc-aae0-4d73-bd31-43f73101e7c0",
+        name: "Lowes",
+        items: [
+            { id: "960cbbcf-89a0-4d79-aa8e-56abbc15eacc", name: "Workbench" },
+            { id: "d3edf796-6449-4931-a777-ff66965a025b", name: "Hammer" },
+        ],
+        tint: 3,
+    },
+];
+
 export const Balance = () => {
+
+    const [stores, setStores] = useState(DATA);
+
     const {assets, getAssets} = useAssets();
     const isSmallHeightScreen = useMediaQuery("(max-height: 500px)");
     const isLargeWidthScreen = useMediaQuery("(min-width: 801px)");
@@ -57,6 +93,36 @@ export const Balance = () => {
             console.error(error.message);
         }
     };
+    function StoreList({ name, items, id }) {
+        return (
+            <Droppable droppableId={id}>
+                {(provided) => (
+                    <div {...provided.droppableProps} ref={provided.innerRef}>
+                        <div className="store-container">
+                            <h3>{name}</h3>
+                        </div>
+                        <div className="items-container">
+                            {items.map((item, index) => (
+                                <Draggable draggableId={item.id} index={index} key={item.id}>
+                                    {(provided) => (
+                                        <div
+                                            className="item-container"
+                                            {...provided.dragHandleProps}
+                                            {...provided.draggableProps}
+                                            ref={provided.innerRef}
+                                        >
+                                            <h4>{item.name}</h4>
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    </div>
+                )}
+            </Droppable>
+        );
+    }
 
     return (
         <Box sx = {{
@@ -82,30 +148,28 @@ export const Balance = () => {
                      console.log("DragDropContext drag-drop event")
                     }}
                 >
-                    <Droppable droppableId="assetsList" type = "groupe">
+
+                    <Droppable droppableId="ROOT" type="group">
                         {(provided) => (
-                            <div className="assetsList" {...provided.droppableProps} ref={provided.innerRef}
-                                //  sx = {{
-                                //     display: "flex",
-                                //     flexDirection: "column",
-                                //     overflowY: "auto",
-                                //     width: "100%",
-                                //     py: 1,
-                                //     gap: 0.5
-                                // }}
-                            >
-                                {assets.map((asset, index) => {
-                                    return (
-                                        <Draggable draggableId={asset.id}  key = {asset.id} index={index}>
-                                            {(provided) => (
-                                                <Asset asset = {asset}
-                                                       ref={provided.innerRef}
-                                                       {...provided.draggableProps}
-                                                       {...provided.dragHandleProps}/>
-                                            )}
-                                        </Draggable>
-                                    )
-                                })}
+                            <div {...provided.droppableProps} ref={provided.innerRef}>
+                                {assets.map((store, index) => (
+                                    <Draggable
+                                        draggableId={store.id}
+                                        index={index}
+                                        key={store.id}
+                                    >
+                                        {(provided) => (
+                                            <div
+                                                {...provided.dragHandleProps}
+                                                {...provided.draggableProps}
+                                                ref={provided.innerRef}
+                                            >
+                                                <Asset asset = {store}/>
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
                             </div>
                         )}
                     </Droppable>
