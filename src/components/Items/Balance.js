085a1a10c8b4af40b69b56852d24479a35236acc
BorisZelikman <oborzelik@gmail.com>
2023-10-17 01:18:38 +0300

    store index but failed

--- src/components/Items/Balance.js
+++ src/components/Items/Balance.js
@@ -14,44 +14,8 @@ import {Grid} from "@mui/material";
 import {getExchangeRates} from "../../data/exchangeMethods";
 import { DragDropContext, Droppable, Draggable  } from 'react-beautiful-dnd';
 
-const DATA = [
-    {
-        id: "0e2f0db1-5457-46b0-949e-8032d2f9997a",
-        name: "Walmart",
-        items: [
-            { id: "26fd50b3-3841-496e-8b32-73636f6f4197", name: "3% Milk" },
-            { id: "b0ee9d50-d0a6-46f8-96e3-7f3f0f9a2525", name: "Butter" },
-        ],
-        tint: 1,
-    },
-    {
-        id: "487f68b4-1746-438c-920e-d67b7df46247",
-        name: "Indigo",
-        items: [
-            {
-                id: "95ee6a5d-f927-4579-8c15-2b4eb86210ae",
-                name: "Designing Data Intensive Applications",
-            },
-            { id: "5bee94eb-6bde-4411-b438-1c37fa6af364", name: "Atomic Habits" },
-        ],
-        tint: 2,
-    },
-    {
-        id: "25daffdc-aae0-4d73-bd31-43f73101e7c0",
-        name: "Lowes",
-        items: [
-            { id: "960cbbcf-89a0-4d79-aa8e-56abbc15eacc", name: "Workbench" },
-            { id: "d3edf796-6449-4931-a777-ff66965a025b", name: "Hammer" },
-        ],
-        tint: 3,
-    },
-];
-
 export const Balance = () => {
-
-    const [stores, setStores] = useState(DATA);
-
-    const {assets, getAssets} = useAssets();
+    const {assets, getAssets, setAssets, updateAssetsInFirestore} = useAssets();
     const isSmallHeightScreen = useMediaQuery("(max-height: 500px)");
     const isLargeWidthScreen = useMediaQuery("(min-width: 801px)");
     const navigate = useNavigate();
@@ -66,7 +30,6 @@ export const Balance = () => {
             getRatesForCurrency("ILS")
         }
     }, []);
-
 
     const totals = assets.reduce((acc, asset) => {
         const {currency, amount} = asset;
@@ -93,37 +56,23 @@ export const Balance = () => {
             console.error(error.message);
         }
     };
-    function StoreList({ name, items, id }) {
-        return (
-            <Droppable droppableId={id}>
-                {(provided) => (
-                    <div {...provided.droppableProps} ref={provided.innerRef}>
-                        <div className="store-container">
-                            <h3>{name}</h3>
-                        </div>
-                        <div className="items-container">
-                            {items.map((item, index) => (
-                                <Draggable draggableId={item.id} index={index} key={item.id}>
-                                    {(provided) => (
-                                        <div
-                                            className="item-container"
-                                            {...provided.dragHandleProps}
-                                            {...provided.draggableProps}
-                                            ref={provided.innerRef}
-                                        >
-                                            <h4>{item.name}</h4>
-                                        </div>
-                                    )}
-                                </Draggable>
-                            ))}
-                            {provided.placeholder}
-                        </div>
-                    </div>
-                )}
-            </Droppable>
-        );
-    }
 
+    const  handleDragDrop = (results)=>{
+        const {source, destination, type}=results;
+        // if (!destination) return;
+        // if (source.droppableId===destination.droppableId && source.index===destination.index) return;
+        if (type==="group"){
+            const reorderedAssets=[...assets];
+            const [removedAsset]=reorderedAssets.splice(source.index,1);
+            reorderedAssets.splice(destination.index,0,removedAsset);
+            console.table( assets);
+            console.table( reorderedAssets);
+            console.log (results)
+    //        console.table( getChangedAssets(assets, reorderedAssets));
+            //updateAssetsInFirestore(reorderedAssets)
+            return setAssets(reorderedAssets)
+        }
+    };
     return (
         <Box sx = {{
             display: "flex",
@@ -144,22 +93,12 @@ export const Balance = () => {
 
 
                 <DragDropContext
-                    onDragEnd={()=>{
-                        console.log("DragDropContext drag-drop event")
-                    }}
+                    onDragEnd={handleDragDrop}
                 >
 
                     <Droppable droppableId="ROOT" type="group">
                         {(provided) => (
-                            <Box {...provided.droppableProps} ref={provided.innerRef}
-                                 sx = {{
-                                     display: "flex",
-                                     flexDirection: "column",
-                                     overflowY: "auto",
-                                     width: "100%",
-                                     py: 1,
-                                     gap: 0.5,
-                                 }}>
+                            <Box className="assetBox" {...provided.droppableProps} ref={provided.innerRef}>
                                 {assets.map((asset, index) => (
                                     <Draggable
                                         draggableId={asset.id}
--- src/hooks/useAssets.js
+++ src/hooks/useAssets.js
@@ -11,6 +11,9 @@ export const useAssets = () => {
             ...doc.data(),
             id: doc.id
         }));
+        // Sort filteredData by the 'index' field
+       // filteredData.sort((a, b) => a.index - b.index);
+
         setAssets(filteredData);
     };
 
@@ -52,8 +55,38 @@ export const useAssets = () => {
         }
     };
 
+    const getChangedAssets = (originalAssets, reorderedAssets) => {
+        return reorderedAssets.filter((reorderedAsset, index) => {
+            const originalAsset = originalAssets[index];
+            return reorderedAsset.index !== originalAsset.index;
+        });
+    }
+
+    const updateChangedAssetsIndexes = async (userId, reorderedAssets) => {
+        try {
+        }
+        catch (err) {
+            console.error("updateChangedAssetsIndexes:", reorderedAssets);
+        }
+    };
+    const updateAssetsInFirestore = async (userId, assetsArray) => {
+        const collectionRef = collection(db, 'users', userId, 'assets');
+
+        // Iterate over the array and update each document in Firestore
+        for (const asset of assetsArray) {
+            const { id, ...rest } = asset;
+            const assetDocRef = doc(collectionRef, id);
+
+            // Update the document with the new data
+            await updateDoc(assetDocRef, rest);
+        }
+      //  await getAssets(userId);
+
+    };
     return {
         assets,
+        setAssets,
+        getChangedAssets,
         getAssets,
         addAsset,
         deleteAsset,
